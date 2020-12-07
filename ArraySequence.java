7de5444a024dba50f6460cc4db9056096a73a3c0
Elliot <ebossi10@stuy.edu>
2020-12-07 13:53:25 -0500

    first thing worked, moving to second construcotr

--- ArraySequence.java
+++ ArraySequence.java
@@ -3,6 +3,40 @@ public class ArraySequence implements IntegerSequence{
   int currentIndex;
   int []data;
 
-  /*Construct the sequence by copying values from the other array into the data array*/
-  public ArraySequence(int [] other){  }
+  public ArraySequence(int [] other){
+    data = new int[other.length];
+    int i = 0;
+    while (i < other.length){
+      data[i] = other[i];
+      i += 1;
+    }
+  }
+
+  public void reset(){
+    currentIndex = 0;
+  }
+  public int length(){
+    return data.length;
+  }
+
+  public boolean hasNext(){
+    if (data.length > currentIndex){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  public int next(){
+    int temp = data[currentIndex];
+    if (!(hasNext())){
+      throw new NoSuchElementException();
+    }
+    else{
+      currentIndex = currentIndex + 1;
+      return temp;
+    }
+  }
+
 }
